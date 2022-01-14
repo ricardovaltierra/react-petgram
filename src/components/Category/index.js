53147381c04a6a53a0a5ec5e1ce477e768691946
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-13 21:48:33 -0600

    Replace anchors with Links and update required paths

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Anchor, Image } from './styles'
+import React from 'react'
+import { Link, Image } from './styles'
 
 const DEAFULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
 
-export const Category = ({ cover =  DEAFULT_IMAGE, path, emoji = '?' }) => (
-  <Anchor href={path}>
+export const Category = ({ cover = DEAFULT_IMAGE, path = '#', emoji = '?' }) => (
+  <Link to={path}>
     <Image src={cover} />
     {emoji}
-  </Anchor>
+  </Link>
 )
