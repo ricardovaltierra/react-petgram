63a0154c920ad6c199e7ad7b5f0e253e0f7e182f
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-14 17:14:56 -0600

    Catch JWT to save in sessionStorage for persistence

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,13 +1,12 @@
-import React, { Fragment } from 'react'
-import Context from '../Context'
+import React, { Fragment, useContext } from 'react'
+import { Context } from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
 import { LoginMutation } from '../container/LoginMutation'
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {
-      ({ activateAuth }) => {
+export const NotRegisteredUser = () => {
+  const { activateAuth } = useContext(Context)
+
         return (
           <Fragment>
             <RegisterMutation>
@@ -16,7 +15,10 @@ export const NotRegisteredUser = () => (
                   const onSubmit = ({ email, password }) => {
                     const input = { email, password }
                     const variables = { input }
-                    register({ variables }).then(activateAuth)
+                    register({ variables }).then(({ data }) => {
+                      const { signup } = data
+                      activateAuth(signup)
+                    })
                   }
 
                   const errorMsg = error && 'El usuario ya existe o hay algún problema.'
@@ -31,7 +33,10 @@ export const NotRegisteredUser = () => (
                   const onSubmit = ({ email, password }) => {
                     const input = { email, password }
                     const variables = { input }
-                    login({ variables }).then(activateAuth)
+                    login({ variables }).then(({ data }) => {
+                      const { login } = data
+                      activateAuth(login)
+                    })
                   }
 
                   const errorMsg = error && 'Ha habido un problema con tus credenciales.'
@@ -41,8 +46,5 @@ export const NotRegisteredUser = () => (
               }
             </LoginMutation>
           </Fragment>
-        )
-      }
-    }
-  </Context.Consumer>
-)
+  )
+}
