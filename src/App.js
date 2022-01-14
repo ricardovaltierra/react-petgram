1f9ecc1c830c089c33a6b83df5fee263e0f4e4c9
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-14 11:39:52 -0600

    Add protected routes and style to active pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { Logo } from './components/Logo'
 import { NavBar } from './components/Navbar'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -16,6 +23,21 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
