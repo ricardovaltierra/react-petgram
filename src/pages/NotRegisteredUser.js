2ca964f1af54edf6e989a1d1e99716448b908fcf
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-14 16:41:12 -0600

    Implement login mutation wrapping login user form

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
@@ -24,7 +25,21 @@ export const NotRegisteredUser = () => (
                 }
               }
             </RegisterMutation>
-            <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+            <LoginMutation>
+              {
+                 (login, { data, loading, error }) => {
+                  const onSubmit = ({ email, password }) => {
+                    const input = { email, password }
+                    const variables = { input }
+                    login({ variables }).then(activateAuth)
+                  }
+
+                  const errorMsg = error && 'Ha habido un problema con tus credenciales.'
+
+                  return <UserForm disabled={loading} error={errorMsg} title='Iniciar Sesión' onSubmit={onSubmit} />
+                }
+              }
+            </LoginMutation>
           </Fragment>
         )
       }
