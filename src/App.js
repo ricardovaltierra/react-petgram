62859a38ecf3ba39e08d463105009e4ebf100ca7
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-17 18:13:09 -0600

    Add page when not found route and add reouter redirection when logged out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Router } from '@reach/router'
-import Context from './Context'
+import React, { useContext } from 'react'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/Navbar'
@@ -9,32 +9,27 @@ import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect noThrow from='/favs' to='/login' />}
+        {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+        {isAuth && <Redirect noThrow from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
 
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-              : <Router>
-                <NotRegisteredUser path='/favs' />
-                <NotRegisteredUser path='/user' />
-              </Router>
-        }
-      </Context.Consumer>
       <NavBar />
     </div>
   )
