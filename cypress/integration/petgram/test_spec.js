90989952189dc2c4847b7d2dcf53e9bfd8512d34
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-01-18 11:45:26 -0600

    Update cypress baseUrl and add app basic specs

--- cypress/integration/petgram/test_spec.js
+++ cypress/integration/petgram/test_spec.js
@@ -1,7 +1,24 @@
-/* global describe, it, expect */
+/* global describe, it, cy */
 
-describe('My frist test', function () {
-  it('para ver si funciona', function () {
-    expect(true).to.equal(false)
+describe('Petgram', function () {
+  it('Visits home page', function () {
+    cy.visit('/')
+  })
+
+  it('Navigates to a category and show photos', function () {
+    cy.visit('/pet/1')
+    cy.get('article')
+  })
+
+  it('Clicks on the navbar to go home path', function() {
+    cy.visit('/pet/1')
+
+    cy.get('nav a').first().click()
+    cy.url().should('include', '/')
+  })
+
+  it('Takes unregistered users to login/register forms when favs clicked', function() {
+    cy.visit('/favs')
+    cy.get('form').should('have.length', 2)
   })
 })
